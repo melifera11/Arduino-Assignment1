7defde07c3235e0ec3d85f084857f8551cd88e87
melifera11 <gyamfiafriyiemelissa@gmail.com>
2026-06-20 20:15:17 +0000

    final countdown sketch

---
+++ sketch_TheAssignment1.ino
@@ -0,0 +1,33 @@
+int startValue = 5;
+int ledPin = 13;
+ 
+ void flashLED(int times){
+  int flashCount = 0;
+  while (flashCount<times){
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);
+    flashCount++;
+  }
+ }
+  void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("=== Smart Countdown Starting ===");
+  int count = startValue;
+
+  while(count>0){
+  Serial.print("Count:");
+  Serial.println(count);
+  flashLED(count);
+  delay(1000);
+  count--;
+  }
+Serial.println("=== Countdown Complete");
+}
+
+void loop() {
+
+
+}
